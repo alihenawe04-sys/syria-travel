70edef33ceae0773e50973ce0491c63624122f3b
alihenawe04-sys <alihenawe04@gmail.com>
2026-07-13 16:03:02 +0300

    Fix uploads static path to use config.upload.dir for Render disk mount

--- server.js
+++ server.js
@@ -231,7 +231,7 @@ app.use(express.static(path.join(__dirname), {
   maxAge: isProd ? '1d' : 0,
   etag: false,
 }));
-app.use('/uploads', express.static(path.join(__dirname, 'public', 'uploads'), {
+app.use('/uploads', express.static(config.upload.dir, {
   maxAge: isProd ? '30d' : 0,
 }));
 
